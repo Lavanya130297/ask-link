650d70c0681c81010e765321f563b75f06409a4c
Lavanya130297 <lavanya.chandran1302@gmail.com>
2026-02-02 14:40:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function yesClick() {
+  document.getElementById("response").innerText =
+    "That made me smile 😊";
+}
+
+function noClick() {
+  document.getElementById("response").innerText =
+    "That’s okay, maybe later 🙂";
+}
